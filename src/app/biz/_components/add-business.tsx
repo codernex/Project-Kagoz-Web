673363323152df6345899eade6650860dfadd849
Borhan Uddin <borhan.dev@gmail.com>
2024-12-03 00:20:35 +0600

    Formatter Update

--- src/app/biz/_components/add-business.tsx
+++ src/app/biz/_components/add-business.tsx
@@ -1,59 +1,87 @@
-"use client"
+"use client";
 import { TextInput } from "@/components/shared/text-input";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Form } from "@/components/ui/form";
 import { useAddBusinessModal } from "@/hooks/addBusinessModal";
 import { useRegisterBusinessMutation } from "@/redux/api/business";
 import { useForm } from "react-hook-form";
 
-export default function AddBusiness({ onOpenChange }: {
-    onOpenChange?: boolean
+export default function AddBusiness({
+  onOpenChange,
+}: {
+  onOpenChange?: boolean;
 }) {
-    /**
-     * State Management Hooks
-     */
-    const { open, setOpen } = useAddBusinessModal()
+  /**
+   * State Management Hooks
+   */
+  const { open, setOpen } = useAddBusinessModal();
 
-    /**
-     * Api Mutation & Queries
-     */
-    const [registerBusiness] = useRegisterBusinessMutation()
+  /**
+   * Api Mutation & Queries
+   */
+  const [registerBusiness] = useRegisterBusinessMutation();
 
-    /**
-     * Form Initialization
-     */
-    const form = useForm({
-        defaultValues: {
-            name: '',
-            mobile: '',
-            email: ''
-        }
-    })
+  /**
+   * Form Initialization
+   */
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      mobile: "",
+      email: "",
+    },
+  });
 
-    return (
-        <div>
-            <Dialog open={open} onOpenChange={onOpenChange ? setOpen : () => { }}>
-                <DialogContent className="max-w-5xl">
-                    <DialogHeader>
-                        <DialogTitle className="!text-black text-mdx">Add new Business</DialogTitle>
-                    </DialogHeader>
+  return (
+    <div>
+      <Dialog open={open} onOpenChange={onOpenChange ? setOpen : () => {}}>
+        <DialogContent className="max-w-5xl">
+          <DialogHeader>
+            <DialogTitle className="!text-black text-mdx">
+              Add new Business
+            </DialogTitle>
+          </DialogHeader>
 
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(d => {
-                            registerBusiness(d)
-                            setOpen(false)
-                        })} className="space-y-4">
-                            <TextInput control={form.control} name="name" label="Business Name" required placeholder="Business Name" />
-                            <TextInput control={form.control} name="mobile" label="Business Mobile" required placeholder="Business Mobile" />
-                            <TextInput control={form.control} name="email" label="Business Email" required placeholder="Business Email" />
-                            <Button className="bg-black rounded-xs">
-                                Submit
-                            </Button>
-                        </form>
-                    </Form>
-                </DialogContent>
-            </Dialog>
-        </div>
-    )
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit((d) => {
+                registerBusiness(d);
+                setOpen(false);
+              })}
+              className="space-y-4"
+            >
+              <TextInput
+                control={form.control}
+                name="name"
+                label="Business Name"
+                required
+                placeholder="Business Name"
+              />
+              <TextInput
+                control={form.control}
+                name="mobile"
+                label="Business Mobile"
+                required
+                placeholder="Business Mobile"
+              />
+              <TextInput
+                control={form.control}
+                name="email"
+                label="Business Email"
+                required
+                placeholder="Business Email"
+              />
+              <Button className="bg-black rounded-xs">Submit</Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
