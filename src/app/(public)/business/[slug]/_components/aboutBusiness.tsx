5b03bf72af1ad4de3dec3bd1ffde5e1517c5317a
Borhan Uddin <borhan.dev@gmail.com>
2025-01-02 18:33:51 +0600

    Ref: Update

--- src/app/(public)/business/[slug]/_components/aboutBusiness.tsx
+++ src/app/(public)/business/[slug]/_components/aboutBusiness.tsx
@@ -7,8 +7,6 @@ import { useParams } from "next/navigation";
 export const AboutBusiness = () => {
   const { slug } = useParams() as { slug: string }
   const { data } = useGetBusinessBySlugQuery(slug)
-  console.log(data?.primaryCategory, data?.subcategories);
-
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2">
@@ -26,7 +24,7 @@ export const AboutBusiness = () => {
           <div className="space-y-smd">
             <div className="flex items-center space-x-xsm">
               <div className="h-[4rem] w-[4rem] border border-borderColor rounded-full flex items-center justify-center">
-                <Image src={appendApi(data?.primaryCategory.iconUrl || '')} alt={data?.primaryCategory.name || ''} width={24} height={24} />
+                <Image src={appendApi(data?.primaryCategory?.iconUrl || '')} alt={data?.primaryCategory.name || ''} width={24} height={24} />
               </div>
               <div>
                 <h3 className="font-bold text-black text-smd">{data?.primaryCategory.name}</h3>
@@ -39,7 +37,7 @@ export const AboutBusiness = () => {
               return (
                 <div key={index} className="flex items-center space-x-xsm">
                   <div className="h-[4rem] w-[4rem] border border-borderColor rounded-full flex items-center justify-center">
-                    <Image src={appendApi(c.iconUrl || '')} alt={c.name || ''} width={24} height={24} />
+                    <Image src={appendApi(c?.iconUrl || '')} alt={c.name || ''} width={24} height={24} />
                   </div>
                   <div>
                     <h3 className="font-bold text-black text-smd">{c.name}</h3>
