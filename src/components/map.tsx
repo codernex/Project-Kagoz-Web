6629aa2781c95088c51c890216f88706384078db
Borhan Uddin <borhan.dev@gmail.com>
2025-03-14 19:53:43 +0600

    Modified Data

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,15 +1,10 @@
 "use client";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { Loader } from "lucide-react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 // Component to handle map centering dynamically
 
-const markerIcon = new L.Icon({
-  iconUrl: "/marker-icon.png",
-  iconSize: [30, 40] as L.PointExpression,
-});
+
 
 const Map: React.FC<{
   lat: number;
@@ -17,8 +12,6 @@ const Map: React.FC<{
   isLoading: boolean;
   cid:string
 }> = ({ lat, lng, isLoading,cid }) => {
-
-  console.log(lat, lng)
   return (
     <div className="w-full h-[40vh]">
       <div
@@ -33,33 +26,6 @@ const Map: React.FC<{
       </iframe>`
         }}
       />
-      {/* <MapContainer
-        center={[lat, lng]}
-        zoom={15}
-        scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          icon={markerIcon}
-          position={[lat, lng]}
-
-        >
-          <Popup>
-            <p>{"Unknown Dealer"}</p>
-          </Popup>
-        </Marker>
-      </MapContainer> */}
-      {/* Optional loading spinner */}
-
-      {isLoading && (
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-[rgba(0,0,0,0.2)] z-[9999]">
-          <Loader size={38} className="animate-spin text-primary" />
-        </div>
-      )}
     </div>
   );
 };
