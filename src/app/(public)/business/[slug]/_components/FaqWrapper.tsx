133e3be96ab69a22bd65f6e9a173812c10f91e57
Borhan Uddin <borhan.dev@gmail.com>
2025-10-16 11:37:59 +0600

    feat: new updates

--- src/app/(public)/business/[slug]/_components/FaqWrapper.tsx
+++ src/app/(public)/business/[slug]/_components/FaqWrapper.tsx
@@ -1,22 +1,21 @@
-"use client"
-import Faq from "@/components/shared/faq"
-import { useGetFaqsQuery } from "@/redux/api"
-import { useParams } from "next/navigation"
+"use client";
+import Faq from "@/components/shared/faq";
+import { useGetFaqsQuery } from "@/redux/api";
+import { useParams } from "next/navigation";
 
 export const FaqBusinessWrapper = () => {
-    const { slug } = useParams() as { slug: string }
-    const { data } = useGetFaqsQuery(slug)
-    if (!data?.length) {
-        return null
-    }
-    return (
-        <>
-            <div className="space-y-sm">
-                <h2 className="text-mdx font-bold text-black">{"FAQ's"}</h2>
-                <Faq faqs={data} />
-            </div>
-            <hr className="border-[#EEEDED]" />
-        </>
-
-    )
-}
+  const { slug } = useParams() as { slug: string };
+  const { data, isLoading } = useGetFaqsQuery(slug);
+  if (!data?.length) {
+    return null;
+  }
+  return (
+    <>
+      <div className="space-y-sm">
+        <h2 className="text-mdx font-bold text-black">{"FAQ's"}</h2>
+        <Faq faqs={data} isLoading={isLoading} />
+      </div>
+      <hr className="border-[#EEEDED]" />
+    </>
+  );
+};
--- src/app/biz/[slug]/dashboard/info/_components/faq.tsx
+++ src/app/biz/[slug]/dashboard/info/_components/faq.tsx
@@ -1,86 +1,104 @@
-"use client"
-import FAQ from "@/app/(public)/(home)/section/faq"
-import Faq from "@/components/shared/faq"
-import { Button } from "@/components/ui/button"
-import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { useAddFaqMutation, useGetFaqsQuery } from "@/redux/api"
-import { PlusIcon } from "lucide-react"
-import { useParams } from "next/navigation"
-import { useForm } from "react-hook-form"
+"use client";
+import FAQ from "@/app/(public)/(home)/section/faq";
+import Faq from "@/components/shared/faq";
+import { Button } from "@/components/ui/button";
+import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useAddFaqMutation, useGetFaqsQuery } from "@/redux/api";
+import { PlusIcon } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 export const Faqs = () => {
-    const { slug } = useParams() as { slug: string }
-    const { data } = useGetFaqsQuery(slug)
-    const [addFaq] = useAddFaqMutation()
-    const form = useForm({
-        defaultValues: {
-            faqs: [
-                {
-                    question: '',
-                    answer: ''
-                }
-            ]
-        }
-    })
-    return (
-        <div className="p-6 space-y-8 text-black bg-white rounded-lg shadow"
+  const { slug } = useParams() as { slug: string };
+  const { data, isLoading } = useGetFaqsQuery(slug);
+  const [addFaq] = useAddFaqMutation();
+  const form = useForm({
+    defaultValues: {
+      faqs: [
+        {
+          question: "",
+          answer: "",
+        },
+      ],
+    },
+  });
+  return (
+    <div className="p-6 space-y-8 text-black bg-white rounded-lg shadow">
+      <h2 className="text-[2.4rem] font-semibold">{"FAQ's"}</h2>
+      <Faq faqs={data} isLoading={isLoading} />
+
+      <Form {...form}>
+        <form
+          className="space-y-6"
+          onSubmit={form.handleSubmit((d) => {
+            addFaq({ slug, ...d });
+            form.reset();
+          })}
         >
-            <h2 className="text-[2.4rem] font-semibold">{"FAQ's"}</h2>
-            <Faq faqs={data} />
+          <div>
+            <div className="flex flex-col gap-8">
+              {form.watch("faqs").map((_, index) => {
+                return (
+                  <div key={index} className="space-y-3">
+                    <FormField
+                      control={form.control}
+                      name={`faqs.${index}.question`}
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Question</FormLabel>
+                            <Input
+                              placeholder="Test message"
+                              className="placeholder:text-muted"
+                              {...field}
+                            />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                    <FormField
+                      control={form.control}
+                      name={`faqs.${index}.answer`}
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>Answer</FormLabel>
+                            <Textarea
+                              placeholder="Faq"
+                              className="placeholder:text-muted"
+                              {...field}
+                            />
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <Button
+              onClick={() =>
+                form.setValue("faqs", [
+                  ...form.watch("faqs"),
+                  { question: "", answer: "" },
+                ])
+              }
+              type="button"
+              variant="outline"
+              className="mt-4 border-primary text-primary hover:text-primary"
+            >
+              <PlusIcon />
+              <span>Add FAQ</span>
+            </Button>
+          </div>
 
-            <Form {...form}>
-                <form className="space-y-6" onSubmit={form.handleSubmit((d) => {
-                    addFaq({ slug, ...d })
-                    form.reset()
-                })}>
-                    <div>
-                        <div className="flex flex-col gap-8">
-                            {
-                                form.watch('faqs').map((_, index) => {
-                                    return (
-                                        <div key={index} className="space-y-3">
-                                            <FormField control={form.control} name={`faqs.${index}.question`} render={({ field }) => {
-
-                                                return (
-                                                    <FormItem>
-                                                        <FormLabel>Question</FormLabel>
-                                                        <Input placeholder="Test message" className="placeholder:text-muted" {...field} />
-                                                    </FormItem>
-                                                )
-                                            }} />
-                                            <FormField control={form.control} name={`faqs.${index}.answer`} render={({ field }) => {
-
-                                                return (
-                                                    <FormItem>
-                                                        <FormLabel>Answer</FormLabel>
-                                                        <Textarea placeholder="Faq" className="placeholder:text-muted" {...field} />
-                                                    </FormItem>
-                                                )
-                                            }} />
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                        <Button onClick={() => form.setValue('faqs', [...form.watch('faqs'), { question: '', answer: '' }])} type="button" variant="outline" className="mt-4 border-primary text-primary hover:text-primary">
-                            <PlusIcon />
-                            <span>Add FAQ</span>
-                        </Button>
-                    </div>
-
-                    <div className="flex justify-end">
-                        {
-                            form.getFieldState('faqs').isDirty && (
-                                <Button>
-                                    Save
-                                </Button>
-                            )
-                        }
-                    </div>
-                </form>
-            </Form>
-        </div>
-    )
-}
+          <div className="flex justify-end">
+            {form.getFieldState("faqs").isDirty && <Button>Save</Button>}
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
